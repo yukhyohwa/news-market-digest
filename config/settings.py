99bf98937e3ced8f78e7efca70e3cdf37a224414
yukhyohwa <shawnlu91@gmail.com>
2026-01-12 13:15:20 +0800

    Update: refine RSS feed list in settings.py

--- config/settings.py
+++ config/settings.py
@@ -7,9 +7,8 @@ RSS_FEEDS = [
     "https://techcrunch.com/category/artificial-intelligence/feed/",
     "https://www.theguardian.com/profile/editorial/rss",
     "https://cn.nytimes.com/rss/", 
-    "https://www.france24.com/en/rss",
+#   "https://www.france24.com/en/rss",
     "https://www.lefigaro.fr/rss/figaro_actualites.xml",
     "https://feeds.washingtonpost.com/rss/politics",
-    "https://search.cnbc.com/rs/search/combinedcms/view.xml?partnerId=wrss01&id=100003114",
 ]
  
